43a3fcb871b4c929ba347b5e2f894011cf034ed0
Soren I. Bjornstad <contact@sorenbjornstad.com>
2020-11-12 18:48:11 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a publish-only repository for the public portions of Random Thoughts,
+    Soren Bjornstad’s commonplace book.
+See <https://randomthoughts.sorenbjornstad.com>.
---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+sourcefile="$HOME/random-thoughts.txt"
+tempfile="/tmp/rt-public.txt"
+target="$(dirname "$0")/index.html"
+
+rtgrep -p '.*' "$sourcefile" >"$tempfile"
+vim -E \
+    -c "let g:html_no_progress=1" \
+    -c "set background=light" \
+    -c "set ft=randomthoughts" \
+    -c "runtime syntax/2html.vim" \
+    -c "wqa" \
+    "$tempfile"
+
+python3 -c "
+from bs4 import BeautifulSoup
+
+with open('$tempfile.html') as f:
+    bs = BeautifulSoup(f.read(), features='html5lib')
+
+bs.find('title').string = 'Random Thoughts – Soren Bjornstad'
+
+entry_number_tags = bs.find_all('span', class_='PreProc')
+for tag in entry_number_tags:
+    this_id = tag.string.strip('@').strip('.')
+    tag.attrs['id'] = this_id
+
+with open('$target', 'w') as f:
+    f.write(bs.prettify())
+"
+
+rm -- "${tempfile:?eek}"
